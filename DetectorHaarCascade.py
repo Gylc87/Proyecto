c0595c067e2415750a6f3368d3bdd81a01a378dc
Gylc87 <a19110230@ceti.mx>
2022-06-22 09:00:27 -0500

    commit

--- DetectorHaarCascade.py
+++ DetectorHaarCascade.py
@@ -2,7 +2,7 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-dustinClassif = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cara = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 if cv2.__version__.startswith('2.4'):
     dmf_flag = cv2.cv.CV_HAAR_SCALE_IMAGE
@@ -14,7 +14,7 @@ while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = dustinClassif.detectMultiScale(
+    faces = cara.detectMultiScale(
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
